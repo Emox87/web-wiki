06d220b615405e22c87074ce9c28332597fc3100
Emox87 <tsvetanovemil87@gmail.com>
2024-01-14 14:26:16 +0200

    Define target section elements

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Targeting section elements
+const htmlSection = document.getElementById("html");
+const cssSection = document.getElementById("css");
+const jsSection = document.getElementById("js");
+const reactSection = document.getElementById("react");
+const nodeSection = document.getElementById("node");
+const mongodbSection = document.getElementById("mongodb");
